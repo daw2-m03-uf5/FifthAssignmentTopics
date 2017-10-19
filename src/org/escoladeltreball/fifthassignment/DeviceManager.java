9c280542e650ded434914f1857a8e388f3c411c3
Jose Mendez Valverde <jmendez1@xtec.cat>
2017-10-19 20:08:07 +0200

    DeviceType lowercase and DeviceManager constructor implemented

--- src/org/escoladeltreball/fifthassignment/DeviceManager.java
+++ src/org/escoladeltreball/fifthassignment/DeviceManager.java
@@ -3,6 +3,9 @@
  */
 package org.escoladeltreball.fifthassignment;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +26,14 @@ public abstract class DeviceManager {
 	 * @throws Exception
 	 */
 	public DeviceManager(final String fileName) throws Exception {
-
+		devices = new ArrayList<>();
+		List<String> devicesStr = Files.readAllLines(Paths.get(fileName));
+		for (String string : devicesStr) {
+			String[] fields = string.split(",");
+			Device device = new Device(Long.parseLong(fields[0]), DeviceType.valueOf(fields[1]), fields[2], fields[3],
+					Double.parseDouble(fields[4]));
+			devices.add(device);
+		}
 	}
 
 	/**
@@ -31,7 +41,8 @@ public abstract class DeviceManager {
 	 * type
 	 * 
 	 * @return Map of a list of devices associated with a device type
-	 * @throws Exception When devices is null
+	 * @throws Exception
+	 *             When devices is null
 	 */
 	public abstract Map<DeviceType, List<Device>> getMapByType() throws Exception;
 
@@ -40,7 +51,8 @@ public abstract class DeviceManager {
 	 * brand
 	 * 
 	 * @return Map of a list of devices associated with a device brand
-	 * @throws Exception When devices is null
+	 * @throws Exception
+	 *             When devices is null
 	 */
 	public abstract Map<String, List<Device>> getMapByBrand() throws Exception;
 
@@ -48,16 +60,19 @@ public abstract class DeviceManager {
 	 * This method returns a set of brands
 	 * 
 	 * @return A set of brands
-	 * @throws Exception When devices is null
+	 * @throws Exception
+	 *             When devices is null
 	 */
 	public abstract Set<String> getSetByBrands() throws Exception;
 
 	/**
 	 * This method returns a sorted list of devices according to a comparator
 	 * 
-	 * @param comparator The sorting field
+	 * @param comparator
+	 *            The sorting field
 	 * @return A sorted list of devices according to a comparator
-	 * @throws Exception When devices null
+	 * @throws Exception
+	 *             When devices null
 	 */
 	public abstract List<Device> getSortedList(Comparator<Device> comparator) throws Exception;
 
@@ -65,7 +80,8 @@ public abstract class DeviceManager {
 	 * This method returns the cheapest device of every type in a map
 	 * 
 	 * @return The cheapest device of every type in a map
-	 * @throws Exception When devices is null
+	 * @throws Exception
+	 *             When devices is null
 	 */
 	public abstract Map<DeviceType, Double> findCheapestDeviceOfEachType() throws Exception;
 
